25995fee35ec70ecea08c46471df35eacc80430d
Ronald Magno <dlanoronald123@gmail.com>
2021-08-28 21:26:15 +0800

    Update app js file

--- smooth-scroll/src/pages/index.js
+++ smooth-scroll/src/pages/index.js
@@ -3,6 +3,9 @@ import Sidebar from '../components/Sidebar'
 import Navbar from '../components/Navbar'
 import HeroSection from '../components/HeroSection'
 import InfoSection from '../components/InfoSection'
+import { homeObjOne, homeObjTwo, homeObjThree } from '../components/InfoSection/Data'
+import Services from '../components/Services'
+import Footer from '../components/Footer'
 
 
 const Home = () => {
@@ -15,7 +18,11 @@ const Home = () => {
 			<Sidebar isOpen={isOpen} toggle={toggle} />	
 			<Navbar toggle={toggle} />
 			<HeroSection />
-			<InfoSection />
+			<InfoSection {...homeObjOne} />
+			<InfoSection {...homeObjTwo} />
+			<Services />
+			<InfoSection {...homeObjThree} />
+			<Footer/>
 		</>
 	)
 }
