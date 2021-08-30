ca0a9c02ba4eef6bd0021464376592c4314af846
Ronald Magno <dlanoronald123@gmail.com>
2021-08-30 18:28:17 +0800

    Added signin Page

---
+++ smooth-scroll/src/components/SignIn/SigninElements.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const SignIn = () => {
+	return (
+		<>
+			<Container>
+				 	<FormWrap>
+						 <Icon to='/'>dolla</Icon>
+						 <FormContent>
+							 <Form actio="#">
+								 <FormH1>Sign in to your account</FormH1>
+							 </Form>
+						 </FormContent>
+					 </FormWrap>
+			</Container>			
+		</>
+	)
+}
+
+export default SignIn
